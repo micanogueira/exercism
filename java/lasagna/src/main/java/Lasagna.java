1d4dff84389428b1a741d1f80a7739135fa67d52
Micaelle Nogueira <mica.lavigne@gmail.com>
2023-10-09 20:35:09 +0200

    java/lasagna: Add improvements based on community solutions

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,21 +1,25 @@
 public class Lasagna {
+
+    private static final int OVEN_TIME = 40; // according to the cooking book, the expected oven time in minutes is 40
+    private static final int LAYER_TIME_PREPARATION = 2; // assuming each layer takes 2 minutes to be prepared
+
     // TODO: define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven() {
-        return 40; // According to the cooking book, the expected oven time in minutes is 40
+        return OVEN_TIME;
     }
 
     // TODO: define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int actualMinutesInOven) {
-        return expectedMinutesInOven() - actualMinutesInOven;
+        return this.expectedMinutesInOven() - actualMinutesInOven;
     }
 
     // TODO: define the 'preparationTimeInMinutes()' method
     public int preparationTimeInMinutes(int numberOfLayers) {
-        return numberOfLayers * 2; // Assuming each layer takes 2 minutes to be prepared
+        return numberOfLayers * LAYER_TIME_PREPARATION;
     }
 
     // TODO: define the 'totalTimeInMinutes()' method
     public int totalTimeInMinutes(int numberOfLayers, int minutesInOven) {
-        return preparationTimeInMinutes(numberOfLayers) + minutesInOven;
+        return this.preparationTimeInMinutes(numberOfLayers) + minutesInOven;
     }
 }
